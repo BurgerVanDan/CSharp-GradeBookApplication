c38835dd68c6a54e3c81ce916565b2d8f4799447
Daniel Ponting <dapontin@microsoft.com>
2018-02-21 14:12:32 +0000

    Chapter 3
    Overriding GetLetterGrade method for Ranked Grade Book

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -11,6 +12,39 @@ namespace GradeBook.GradeBooks
             :base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("You need at least 5 students to complete ranked grading");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            //LINQ - I do not understand the threshold or the linq below
+            var grade = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if(grade[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if(grade[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grade[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grade[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
---
+++ GradeBook/StudentComparerByGrade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class StudentComparerByGrade : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            if (x.AverageGrade < y.AverageGrade)
+            {
+                return 1;
+            }
+            if (x.AverageGrade > y.AverageGrade)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
